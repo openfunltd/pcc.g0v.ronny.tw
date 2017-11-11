97b8ab79bfb7837696289e2f6b9ae9671baf071c
Ronny Wang <ronnywang@gmail.com>
2017-11-11 11:47:00 +0800

    讓搜尋回傳完整資訊

--- webdata/controllers/SearchController.php
+++ webdata/controllers/SearchController.php
@@ -4,20 +4,44 @@ class SearchController extends Pix_Controller
 {
     public function bytitleAction()
     {
+        $start = microtime(true);
+
+        $result = new StdClass;
+        $result->query = strval($_GET['query']);
+        $result->page = intval($_GET['page']) ?: 1;
+
         $curl = curl_init();
         $cmd = array(
             'query' => array(
-                'query_string' => array('query' => sprintf('title:"%s"', $_GET['query'])),
+                'query_string' => array('query' => sprintf('title:"%s"', $result->query)),
             ),
             'size' => 100,
+            'from' => $result->page * 100 - 100,
             'sort' => array('date' => 'desc'),
         );
+
         curl_setopt($curl, CURLOPT_URL, getenv('SEARCH_URL') . '/entry/_search');
         curl_setopt($curl, CURLOPT_HEADER, 0);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($cmd));
         curl_setopt($curl, CURLOPT_TIMEOUT, 10);
         $ret = curl_exec($curl);
-        return $this->json(json_decode($ret));
+        $ret = json_decode($ret);
+
+        $result->total_records = $ret->hits->total;
+        $result->total_pages = ceil($ret->hits->total / 100);
+        $result->took = 0;
+        $match_ids = array();
+        foreach ($ret->hits->hits as $hit) {
+            $match_ids[] = explode('-', $hit->_id, 2);
+        }
+        $result->records = array();
+        foreach (Entity::search(1)->searchIn(array('date', 'filename'), $match_ids) as $entity) {
+            $record = $entity->toArray();
+            $record['brief'] = json_decode($record['brief']);
+            $result->records[] = $record;
+        }
+        $result->took = microtime(true) - $start;
+        return $this->json($result);
     }
 }
