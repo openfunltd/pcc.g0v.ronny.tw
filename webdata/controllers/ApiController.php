fc1f8a74f80a965c39b5585acd65d976f011805f
Ronny Wang <ronnywang@gmail.com>
2018-08-26 18:17:55 +0800

    add searchbycompanyid API

--- webdata/controllers/ApiController.php
+++ webdata/controllers/ApiController.php
@@ -56,6 +56,59 @@ class ApiController extends Pix_Controller
         ));
     }
 
+    public function searchbycompanyidAction()
+    {
+        $start = microtime(true);
+
+        $result = new StdClass;
+        $result->query = strval($_GET['query']);
+        $result->page = intval($_GET['page']) ?: 1;
+
+        $curl = curl_init();
+        $cmd = array(
+            'query' => array(
+                'term' => array('companies.ids' => $result->query),
+            ),
+            'size' => 100,
+            'from' => $result->page * 100 - 100,
+            'sort' => array('date' => 'desc'),
+        );
+
+        curl_setopt($curl, CURLOPT_URL, getenv('SEARCH_URL') . '/entry/_search');
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($cmd));
+        curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+        $ret = curl_exec($curl);
+        $ret = json_decode($ret);
+
+        $result->total_records = $ret->hits->total;
+        $result->total_pages = ceil($ret->hits->total / 100);
+        $result->took = 0;
+        $match_ids = array();
+        $unit_oids = array();
+        foreach ($ret->hits->hits as $hit) {
+            $match_ids[] = explode('-', $hit->_id, 2);
+            $unit_oids[$hit->_source->oid] = $hit->_source->oid;
+        }
+        foreach (Unit::search(1)->searchIn('oid', array_keys($unit_oids)) as $unit) {
+            $unit_oids[$unit->oid] = $unit->name;
+        }
+        $result->records = array();
+        foreach (Entity::search(1)->searchIn(array('date', 'filename'), $match_ids)->order('date DESC') as $entity) {
+            $record = $entity->toArray();
+            $record['unit_id'] = $record['oid'];
+            unset($record['oid']);
+            $record['brief'] = json_decode($record['brief']);
+            $record['unit_name'] = $unit_oids[$record['unit_id']];
+            $record['unit_api_url'] = $this->base . '/api/listbyunit?unit=' . urlencode($record['unit_id']);
+            $record['tender_api_url'] = $this->base . '/api/tender?unit=' . urlencode($record['unit_id']) . '&job_number=' . urlencode($record['job_number']);
+            $result->records[] = $record;
+        }
+        $result->took = microtime(true) - $start;
+        return $this->json($result);
+    }
+
     public function searchbycompanynameAction()
     {
         $start = microtime(true);
