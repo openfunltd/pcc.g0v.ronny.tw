86a0fb87f6c2ca8345aec39adc327504fa64c69d
Ronny Wang <ronnywang@gmail.com>
2023-09-06 16:21:17 +0800

    add swagger page

--- index.php
+++ index.php
@@ -2,5 +2,19 @@
 
 include(__DIR__ . '/webdata/init.inc.php');
 
+class MyDispatcher extends Pix_Controller_Dispatcher
+{
+    public function dispatch($path)
+    {
+        if ($path == '/swagger.json') {
+            header('Content-Type: application/json');
+            header('Access-Control-Allow-Origin: *');
+            readfile(__DIR__ . '/webdata/swagger.json');
+            exit;
+        }
+        return null;
+    }
+}
+Pix_Controller::addDispatcher(new MyDispatcher);
 Pix_Controller::addCommonHelpers();
 Pix_Controller::dispatch(__DIR__ . '/webdata/');
--- webdata/controllers/IndexController.php
+++ webdata/controllers/IndexController.php
@@ -5,4 +5,8 @@ class IndexController extends Pix_Controller
     public function indexAction()
     {
     }
+
+    public function swaggerAction()
+    {
+    }
 }
